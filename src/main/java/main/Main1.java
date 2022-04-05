420d24862a572ba2694e3b9e94986405ff8974dc
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2022-04-05 23:39:17 +0200

    Dodata klasa Main1

---
+++ src/main/java/main/Main1.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import domain.Comic;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("ID: ");
+		Scanner s = new Scanner(System.in);
+		int id = s.nextInt();
+		
+		try {
+			URL url = new URL("https://xkcd.com/" + id + "/info.0.json");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			Gson gson = new GsonBuilder().create();
+			Comic comic = gson.fromJson(in, Comic.class);
+			
+			in.close();
+			System.out.println(comic);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
