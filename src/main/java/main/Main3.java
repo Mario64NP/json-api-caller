3ea5b296338f4e913b82172b975c4821f7122f4d
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2022-04-06 20:52:24 +0200

    Dodata klasa Main3

---
+++ src/main/java/main/Main3.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import domain.Comic;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		
+		try(FileReader in = new FileReader("niz_objekata.json")){
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			List<Comic> comics = Arrays.asList(gson.fromJson(in, Comic[].class));
+			
+			for(Comic c: comics)
+				System.out.println(c);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
